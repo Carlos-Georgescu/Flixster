798c67cb9cfb01f15251aebf10edd55ade9a1feb
Carlos Georgescu <carlosgeorgescu@fb.com>
2022-06-03 16:47:22 -0700

    Adding extra features (only displays stars)

--- app/src/main/java/com/example/flixster/MovieDetailsActivity.java
+++ app/src/main/java/com/example/flixster/MovieDetailsActivity.java
@@ -27,24 +27,28 @@ public class MovieDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
 
-        movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
-        Log.d("MovieDetailsActivity", String.format("Showing details for '%s'", movie.getTitle()));
 
+        ratingPicture = (ImageView) findViewById(R.id.ratingPicture);
         tvTitle = (TextView) findViewById(R.id.tvTitle);
         tvOverview = (TextView) findViewById(R.id.tvOverview);
         rbVoteAverage = (RatingBar) findViewById(R.id.rbVoteAverage);
-        ratingPicture = (ImageView) findViewById(R.id.ratingPicture);
 
-
+        // unwrap the movie passed in via intent, using its simple name as a key
+        movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
         Log.d("MovieDetailsActivity", String.format("Showing details for '%s'", movie.getTitle()));
+
 
         // set the title and overview
         tvTitle.setText(movie.getTitle());
         tvOverview.setText(movie.getOverview());
 
         // vote average is 0..10, convert to 0..5 by dividing by 2
-        float voteAverage = (float) (movie.getVoteAverage());
-        rbVoteAverage.setRating(voteAverage / 2.0f);
+
+       // float voteAverage = movie.getVoteAverage().floatValue;
+       // System.out.println(voteAverage);
+        rbVoteAverage.setMinimumWidth(200);
+        rbVoteAverage.setNumStars(2);
+
 
         Glide.with(this)
                 .load(movie.getBackdropPath())
